8530564674ae311e8cbb11a1e30e84005782ed1f
Juan Alvarado <jalvarado91@gmail.com>
2024-01-30 01:09:56 -0500

    One more

--- example_2/node-operation/src/index.js
+++ example_2/node-operation/src/index.js
@@ -9,31 +9,41 @@ const input_a_k = process.env.INPUT_A ?? "0"
 const input_b_k = process.env.INPUT_B ?? "0"
 const result_loc = process.env.RES_LOC ?? "RES"
 
-const client = await createClient({
-  url: redisUrl
-})
-  .on('error', err => console.error('Redis Client Error', err))
-  .connect();
 
-const input_a = await client.get(`${runId}.${input_a_k}`);
-const input_b = await client.get(`${runId}.${input_b_k}`)
+async function run() {
+  const client = await createClient({
+    url: redisUrl
+  })
+    .on('error', err => console.error('Redis Client Error', err))
+    .connect();
 
-let res = 0
-switch (operation) {
-  case "SUM":
-    res = input_a + input_b
-    break
-  case "SUB":
-    res = input_a - input_b
-    break
-  case "MULT":
-    res = input_a * input_b
-    break
-  default:
-    throw new Error(`Operation ${operation} not supported. Must be "SUM | "SUB" | "MULT"`)
+  const input_a = await client.get(`${runId}.${input_a_k}`);
+  const input_b = await client.get(`${runId}.${input_b_k}`)
+
+  let res = 0
+  switch (operation) {
+    case "SUM":
+      res = input_a + input_b
+      break
+    case "SUB":
+      res = input_a - input_b
+      break
+    case "MULT":
+      res = input_a * input_b
+      break
+    default:
+      throw new Error(`Operation ${operation} not supported. Must be "SUM | "SUB" | "MULT"`)
+  }
+
+  const resK = `${runId}.${result_loc}`
+  await client.set(resK, res);
+
+  await client.disconnect();
 }
 
-const resK = `${runId}.${result_loc}`
-await client.set(resK, res);
-
-await client.disconnect();
+run()
+  .then(() => console.log("Finished successully"))
+  .catch((err) => {
+    console.error(err.Error)
+    process.exitCode = 1
+  })
